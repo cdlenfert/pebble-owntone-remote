ab434c214461a8d610d52d5d8f42c6ffb3750f61
Chris Lenfert <chris+clmbp22m2@codegeek.net>
2026-02-09 11:41:38 -0700

    Robustly parse favorite type tuples (handle string-encoded numbers) and log them

--- src/messaging.c
+++ src/messaging.c
@@ -272,7 +272,16 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
       }
       
       Tuple *type_t = dict_find(iterator, KEY_FAVORITE_TYPE_BASE + i);
-      if (type_t) types[i] = type_t->value->uint8;
+      if (type_t) {
+        // Handle both integer and string-encoded types coming from JS
+        if (type_t->value->cstring) {
+          types[i] = atoi(type_t->value->cstring);
+          APP_LOG(APP_LOG_LEVEL_DEBUG, "favorite type[%d] received as cstring '%s' -> %d", i, type_t->value->cstring, types[i]);
+        } else {
+          types[i] = type_t->value->uint8;
+          APP_LOG(APP_LOG_LEVEL_DEBUG, "favorite type[%d] received as uint8 -> %d", i, types[i]);
+        }
+      }
     }
 
     // Debug: log received favorites
