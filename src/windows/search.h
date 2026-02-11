8e3f32e545139d94357779b80087731013b1e258
Chris Lenfert <chris+clmbp22m2@codegeek.net>
2026-02-11 09:02:31 -0700

    aplite: hide search UI and conditionalize search implementation for Aplite

--- src/windows/search.h
+++ src/windows/search.h
@@ -2,6 +2,8 @@
 #include <pebble.h>
 #include "../message_keys.h"
 
+#if !defined(PBL_PLATFORM_APLITE)
 void search_window_push(void);
+#endif
 void random_window_push(void);
 void results_window_push(int count, char *titles[], char *uris[], ContentType type);
